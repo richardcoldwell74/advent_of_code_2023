3aee3e8fec062c231b566d973e2380d637491a52
richardcoldwell74 <76210848+richardcoldwell74@users.noreply.github.com>
2023-12-09 13:11:30 +0000

    day 6

--- app/day_06/functions/functions.ts
+++ app/day_06/functions/functions.ts
@@ -1,8 +1,37 @@
 export type ReturnValues = { partA: number; partB: number };
 
-export const Solutions = (inputArray: string[]): ReturnValues => {
-  let total = 0;
-  let totalTwo = 0;
+const totalDistance = (timeHolding: number, totalDuration: number) => {
+  return (totalDuration - timeHolding) * timeHolding;
+};
 
-  return { partA: total, partB: totalTwo };
+export const Solutions = (inputArray: string[]): ReturnValues => {
+  const parsed = inputArray
+    .map((line) => line.split(":")[1].trim())
+    .map((nums) => nums.split(/ +/).map((num) => parseInt(num)));
+
+  const total = parsed[0]
+    .map((t: number) =>
+      Array(t)
+        .fill(0)
+        .map((_, t1: number) => totalDistance(t1, t))
+    )
+    .map((race: number[], i: number) => race.filter((d) => d > parsed[1][i]))
+    .reduce((acc: number, v: number[]) => acc * v.length, 1);
+
+  return { partA: total, partB: 0 };
+};
+
+export const SolutionsB = (inputArray: string[]): ReturnValues => {
+  let totalTwo = 0;
+  const parsedPart2 = inputArray.map((line) =>
+    parseInt(line.split(":")[1].replace(/ /g, ""))
+  );
+
+  for (let x = 0; x <= parsedPart2[0]; x++) {
+    const d = totalDistance(x, parsedPart2[0]);
+    if (d > parsedPart2[1]) {
+      totalTwo++;
+    }
+  }
+  return { partA: 0, partB: totalTwo };
 };
--- app/day_06/input/input.ts
+++ app/day_06/input/input.ts
@@ -1,3 +1,5 @@
-export const TestInput = ``;
+export const TestInput = `Time:      7  15   30
+Distance:  9  40  200`;
 
-export const Input = ``;
+export const Input = `Time:        49     87     78     95
+Distance:   356   1378   1502   1882`;
